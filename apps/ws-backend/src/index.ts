e7658ef2108b80098939b1e3d748b2fee9ca7eb6
Manishjoshi <manishjoshi705572@gmail.com>
2025-01-23 02:16:46 +0530

    web socket locgic done except db

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,15 @@
 import { WebSocketServer } from 'ws';
 import jwt, { JwtPayload } from "jsonwebtoken"
 import { JWT_SECRET } from "@repo/backend-common/config"
+import { WebSocket } from 'ws';
 
+interface User {
+    userId  : String | null,
+    rooms   : String[],
+    socket  : WebSocket
+}
+
+const users : User [] = []
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -21,6 +29,7 @@ function checkUser(token : String): String | null {
 
 
 wss.on('connection', function connection(socket , Request) {
+    try{
     const url = Request.url // ws://localhost:3000?token=123123
     if(!url){
         return
@@ -33,9 +42,47 @@ wss.on('connection', function connection(socket , Request) {
         socket.send("invalid token")
         socket.close()
     }
+    // whenever a user is connect 
+    users.push({
+        userId,
+        rooms:[],
+        socket
+    })
     
     socket.on('message', function message(data) {
-    socket.send('something');
-  });
+          const parsedData = JSON.parse(data as unknown as string)
+          const roomId = parsedData.payload.roomId
+          
+          if(parsedData.type == "join_room"){
+            const user = users.find( x => x.socket == socket)
+            user?.rooms.push(roomId)
+          }
 
+          if(parsedData.type == "leave_room"){
+            const user = users.find(x => x.socket == socket)
+            if(!user){
+                return
+            }
+            user.rooms = user.rooms.filter(x => x === JSON.stringify(roomId))
+          }
+
+          if(parsedData.type == "chat"){
+            const room = parsedData.payload.roomId
+            const message = parsedData.payload.message
+            users.forEach( e => {
+                if(e.rooms.includes(room)){
+                    e.socket.send(JSON.stringify({
+                        type : "chat",
+                        payload  : {
+                            message,
+                            room
+                        }
+                    }))
+                }
+            })
+          }
+  });
+    }catch(e){
+        socket.send("error occured")
+    }
 });
